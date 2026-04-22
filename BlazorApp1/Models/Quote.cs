0319739a75e450e98c273577d32e9a54f11aeaaa
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2026-04-23 03:31:56 +0630

    feat: add Quotes

---
+++ BlazorApp1/Models/Quote.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorApp1.Models
+{
+    public class Quote
+    {
+        public int QuoteId { get; set; }
+        public string? Name { get; set; }
+        public string? Author { get; set; }
+
+    }
+}
---
+++ BlazorApp1/Repos/QuoteRepo.cs
@@ -0,0 +1,38 @@
+﻿using BlazorApp1.Models;
+
+namespace BlazorApp1.Repos
+{
+    public static class QuoteRepo
+    {
+        private static readonly List<Quote> _quotes = new List<Quote>
+        {
+            new Quote{Name = "Quote One",Author="susu" ,QuoteId =1},
+            new Quote{Name = "Quote Two",Author="koko",QuoteId =2},
+            new Quote{Name = "Quote Three",Author="jojo",QuoteId=3},
+            new Quote{Name = "Quote Four",Author="mgmg",QuoteId =4}
+        };
+
+        public static List<Quote> AllQuotes()
+        {
+            return _quotes;
+        }
+
+        public static Quote? GetById(int id)
+        {
+            var quote = _quotes.FirstOrDefault(q => q.QuoteId == id);
+            return quote;
+        }
+
+        public static void AddQuote(Quote quote)
+        {
+            _quotes.Add(quote);
+        }
+
+        public static void DeleteQuote(int id)
+        {
+            var quote = _quotes.FirstOrDefault(q=>q.QuoteId == id);
+             _quotes.Remove(quote!);
+            
+        }
+    }
+}
